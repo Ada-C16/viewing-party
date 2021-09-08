7602b590b87a3e47d96179b38804911eed454071
Asli Athman <asliathman@gmail.com>
2021-09-08 18:58:31 -0400

    finished code for wave 1, passed all tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,2 +1,31 @@
-def create_movie(): 
-    pass
+#wave 1
+def create_movie(title, genre, rating): 
+    movie = {}
+    if title and genre and rating:
+        movie["title"] = title
+        movie["genre"] = genre
+        movie["rating"] = rating
+        
+        return movie
+    else:
+        return None
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+
+    return user_data
+
+def watch_movie(user_data, title):
+    watchlist = user_data["watchlist"]
+
+    for i in range(len(watchlist)):
+        if title in watchlist[i].values():
+            movie = watchlist.pop(i)
+            add_to_watched(user_data, movie)
+    
+    return user_data
