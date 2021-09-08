0aa6b271f358960761ed0f73bff2bfa39457f9c5
Rebecca Zuick <rebecca.zuick@gmail.com>
2021-09-08 17:31:11 -0500

    created create movie function

---
+++ viewing_party/party.py
@@ -0,0 +1,18 @@
+def create_movie(movie_title, genre, rating):
+    new_movie = {}
+    new_movie["title"] = movie_title
+    new_movie["genre"] =  genre
+    new_movie["rating"] = rating
+    print(new_movie)
+    return new_movie
+    
+
+
+def add_to_watched():
+    pass
+
+def add_to_watchlist():
+    pass
+
+def watch_move():
+    pass
