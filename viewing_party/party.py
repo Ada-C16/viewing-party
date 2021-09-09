5b6e3d6368749bd96ad7dafc03f77ab90ca1ad8d
Lia Gaetano <lgaetano19@gmail.com>
2021-09-09 14:36:14 -0700

    Created add_to_watched()

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,4 +1,9 @@
 def create_movie(title, genre, rating):
+    '''
+    Creates a new movie profile in dictionary format.
+    Returns movie dict if information is complete.
+    Returns None if any empty fields.
+    '''
     # Create dictonary for movie profile
     movie = {}
     
@@ -13,3 +18,13 @@ def create_movie(title, genre, rating):
     else:
         # If data missing, return None
         return None
+
+def add_to_watched(user_data, movie):
+    '''
+    Adds a movie that a user has viewed to the user's data profile.
+    '''
+    # Append watched-movie data to user_data's watched list
+    user_data['watched'].append(movie)
+    
+    # Return user profile
+    return user_data
