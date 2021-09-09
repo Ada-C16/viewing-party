756c375a8a35e8c331fdb787a07f009dfdd165cf
lain winslow <lain.winslow@gmail.com>
2021-09-09 14:18:53 -0700

    wave 2 tests pass

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,3 +1,4 @@
+# wave 1
 
 def create_movie(title, genre, rating):
     if title and genre and rating:
@@ -16,8 +17,19 @@ def add_to_watchlist(user_data, movie):
 def watch_movie(user_data, title):
     titlesearch = list(filter(lambda m: m["title"] == title, user_data["watchlist"]))
     if titlesearch:
-        movie = titlesearch[0] # assume unique title
+        movie = titlesearch.pop() # assume unique title
         user_data["watchlist"] = list(filter(lambda m: m["title"] != title, user_data["watchlist"]))
         user_data["watched"].append(movie)
     return user_data
+
+# wave 2
+
+def get_watched_avg_rating(user_data):
+    ratings = [m["rating"] for m in user_data["watched"]]
+    return sum(ratings)/len(ratings) if ratings else 0.0
+
+def get_most_watched_genre(user_data):
+    genres = [m["genre"] for m in user_data["watched"]]
+    return max(set(genres), key=genres.count) if genres else None
+
 
