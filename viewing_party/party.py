d8e00ac0de210c8e47b6510b83f55074fbb730bc
Rachael <mcbride.rac@gmail.com>
2021-09-08 20:38:28 -0400

    Added all functions which pass all tests; still needs refactoring

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -53,8 +53,136 @@ def get_most_watched_genre(user_data):
         else:
             genre_dict[genre] = 1
 
-    return max(genre_dict, key=genre_dict.get)   
+    return max(genre_dict, key=genre_dict.get)  
+
+# extra helper method that's not asked for but was made to DRY up code 
+def find_user_and_friend_movies(user_data):
+    '''takes in a user_data dict with keys 'watched' (list of watched movies)
+    and friends (list of friends and movies they've watched); returns two 
+    lists--one that reps movies only user has watched and one that reps 
+    movies only friends have watched'''
+    movies_user_watched = [movie['title'] for movie in user_data['watched']]
+
+    movies_friends_watched = []
+    for friend in user_data['friends']:
+        for movie in friend['watched']:
+            movies_friends_watched.append(movie['title'])
+    
+    return movies_user_watched, movies_friends_watched
+
+def get_unique_watched(user_data):
+    '''takes in a user_data dict with keys 'watched' (list of watched movies)
+    and friends (list of friends and movies they've watched); returns list of 
+    dicts that rep list of movies that user has watched but friends haven't''' 
+    user_movies, friend_movies = find_user_and_friend_movies(user_data)
+    user_unique_movies = set(user_movies) - set(friend_movies)
+    return [{ 'title' : movie } for movie in user_unique_movies]
+    
+def get_friends_unique_watched(user_data):
+    '''takes in a user_data dict with keys 'watched' (list of watched movies)
+    and friends (list of friends and movies they've watched); returns list of 
+    dicts that rep list of movies a friend has watched but user hasn't'''
+    user_movies, friend_movies = find_user_and_friend_movies(user_data)
+    friend_unique_movies = set(friend_movies) - set(user_movies)
+    return [{ 'title' : movie } for movie in friend_unique_movies]
+
+def get_available_recs(user_data):
+    '''takes in a user_data dict with keys 'subscriptions' (list of streaming
+    services), 'watched' (list of movies), and 'friends' (list of 'watched' 
+    dicts that contain titles and streaming service hosts); returns list of 
+    movies that user hasn't watched that friend has watched + shares a 
+    streaming service with the user'''
+    recommendations = []
+
+    for friend in user_data['friends']:
+        for movie in friend['watched']:
+            title, host = movie['title'], movie['host']
+            new_rec = {"title": title, "host": host}
+
+            if title not in find_user_and_friend_movies(user_data)[0] and\
+                host in user_data['subscriptions'] and\
+                new_rec not in recommendations:
+                    recommendations.append(new_rec)
+
+    return recommendations
+
+def get_new_rec_by_genre(user_data):
+    '''takes in a user_data dict with keys 'watched' (list of watched movies)
+    and friends (list of friends and movies they've watched); returns list of 
+    recommendations that user should watch based on genre they watch most'''
+    top_genre = get_most_watched_genre(user_data)
+    recommendations = []
+
+    for friend in user_data['friends']:
+        for movie in friend['watched']:
+            title, genre = movie['title'], movie['genre']
+            new_rec = {"title": title, "genre": genre}
+
+            if title not in find_user_and_friend_movies(user_data)[0] and\
+                genre == top_genre and\
+                new_rec not in recommendations:
+                    recommendations.append(new_rec)
+    
+    return recommendations
+
+def get_rec_from_favorites(user_data):
+    '''takes in a user_data dict with keys 'watched' (list of watched movies),
+    'favorites' (list of fav movies), and friends (list of friends and movies 
+    they've watched); returns list of recommendations that are in users favs
+    that none of user's friends have watched'''
+    recommendations = []
+
+    for movie in get_unique_watched(user_data):
+        if movie in user_data['favorites']:
+            recommendations.append(movie)
+    
+    return recommendations
+
 # sample data
+# sonyas_data = {
+#             "watched": [
+#                 {
+#                     "title": "Title A"
+#                 },
+#                 {
+#                     "title": "Title B"
+#                 },
+#                 {
+#                     "title": "Title C"
+#                 },
+#                 {
+#                     "title": "Title X"
+#                 }
+#             ],
+#             "favorites": [
+#                 {
+#                     "title": "Title A"
+#                 },
+#                 {
+#                     "title": "Title B"
+#                 }
+#             ],
+#             "friends": [
+#                 {
+#                     "watched": [
+#                         {
+#                             "title": "Title B"
+#                         }
+#                     ]
+#                 },
+#                 {
+#                     "watched": [
+#                         {
+#                             "title": "Title C"
+#                         },
+#                         {
+#                             "title": "Title D"
+#                         }
+#                     ]
+#                 }
+#             ]
+#         }
+
 # user_data = {
 #     'watched' : [{
 #             'title': 'Bridesmaids',
