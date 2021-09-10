09f801d7692fada52d70182059258fe7fc2a6250
gabi <annagabsoares@gmail.com>
2021-09-09 21:47:15 -0700

    commit changes

---
+++ tests/scratch.py
@@ -0,0 +1,2 @@
+import pytest
+from viewing_party.party import *
---
+++ viewing_party/party.py
@@ -0,0 +1,36 @@
+
+
+def watch_movie(data_dict, title):
+    pass
+
+
+def add_to_watchlist(user_data, movie):
+    pass
+
+
+def add_to_watched(user_data, movie):
+    pass
+
+def create_movie(movie_title, genre, rating):
+    pass
+
+def get_watched_avg_rating(data_dic):
+    pass
+
+def get_most_watched_genre(data_dict):
+    pass
+
+def get_unique_watched(data_dic):
+    pass
+
+def get_friends_unique_watched(data_dict):
+    pass
+
+def get_available_recs(data_dict):
+    pass
+
+def get_new_rec_by_genre(data):
+    pass
+
+def get_rec_from_favorites(data):
+    pass
