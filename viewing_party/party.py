18a6b863b709bf3cffc9638e8512f3a22a0716f3
Rebecca Zuick <rebecca.zuick@gmail.com>
2021-09-10 16:26:51 -0500

    final update to create movie function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,26 +1,28 @@
 def create_movie(title, genre, rating):
+  if title and genre and rating:
     new_movie = {}
-    if  title == True:
-        new_movie["title"] = title
-    elif genre == True:
-        new_movie["genre"] = genre
-    elif rating == True:
-        new_movie["rating"] = rating
-    else:
-        new_movie  is None
-    return new_movie
-    
+    new_movie["title"] = title
+    new_movie["genre"] = genre
+    new_movie["rating"] = rating
+  else: 
+    new_movie = None
+  return new_movie
     
 
 
 def add_to_watched(user_data, movie):
-    user_data["watched"] = []
-    movie = {
-        "title": "Title A",
-        "genre": "Horror",
-        "rating": 3.5
-    }
-    return user_data["watched"].append(movie)
+    if user_data["watched"] == []:
+        user_data = None
+    else:
+        user_data["watched"] = []
+        movie = {
+            "title": "Title A",
+            "genre": "Horror",
+            "rating": 3.5
+        }
+        (user_data["watched"]).append(movie)
+    return user_data
+    # return user_data
     # return user_data
 
 def add_to_watchlist():
