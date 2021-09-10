b71d79d3b57f3d2f7ce789a12c7db9f8c5309830
Lux Barker <lux.barker13@gmail.com>
2021-09-10 15:58:35 -0700

    pass wave 3 tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -62,3 +62,46 @@ def get_most_watched_genre(user_data):
     genres = [movie["genre"] for movie in user_data["watched"]]
     most_watched = statistics.mode(genres) if genres else None
     return most_watched
+
+
+# wave 3
+
+def create_unique_sets(user_data):
+    """
+    returns two sets, one containing movies the user watched, and one containing movies the friends watched
+    """
+    user_watched = set([movie["title"] for movie in user_data["watched"]])
+    friends_watched = set()
+    for friend in user_data["friends"]:
+        friends_watched.update([movie["title"] for movie in friend["watched"]])
+    return user_watched, friends_watched
+
+
+def get_unique_watched(user_data):
+    """
+    returns list of movies in user_data["watched"] but not in user_data["friends"]
+    """
+    user_watched, friends_watched = create_unique_sets(user_data)
+    unique_movies = list(user_watched - friends_watched)
+
+    unique_movies = [
+        movie for movie in user_data["watched"] if movie["title"] in unique_movies]
+
+    return unique_movies
+
+
+def get_friends_unique_watched(user_data):
+    """
+    returns list of movies in watched by friends but not the user
+    """
+    user_watched, friend_watched = create_unique_sets(user_data)
+    unique_movies = list(friend_watched - user_watched)
+
+    result = []
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie["title"] in unique_movies:
+                result.append(movie)
+                unique_movies.remove(movie["title"])
+
+    return result
