6459945d769a504380d711be5254265763c44954
Lia Gaetano <lgaetano19@gmail.com>
2021-09-09 19:32:22 -0700

    Created watch_movie()

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -24,7 +24,7 @@ def add_to_watched(user_data, movie):
     Adds a movie that a user has viewed to the user's data profile.
     Returns updated user_data dictionary.
     '''
-    # Append watched-movie data to user's watched list
+    # Append watched-movie data to user's 'watched' list
     user_data['watched'].append(movie)
     
     # Return user data
@@ -35,11 +35,41 @@ def add_to_watchlist(user_data, movie):
     Adds a new movie to user's watchlist.
     Returns updated user_data dictionary.
     '''
-    # Append movie to user's watchlist
+    # Append movie to user's 'watchlist'
     user_data['watchlist'].append(movie)
 
     # Return user data
     return user_data
 
-def watch_movie():
-    pass
+def watch_movie(user_data, movie_title):
+    '''
+    Moves movie from user's watchlist to watched-list after viewing.
+    Returns updated user data.
+    '''
+    # Filter user's watchlist, a list of dicitionaries, for movie title.
+    # Cast filter object as a list to retrieve filter's return value
+    movie_watched = list(filter(lambda movie: movie['title'] == movie_title, 
+        user_data['watchlist']))
+
+    # movie_watched is formated as follows:
+        # [{'title': 'Title B', 'genre': 'Action', 'rating': 2.0}]
+    
+    # If movie not in 'watched' list, return user data
+    if movie_watched:
+
+        # Append watched movie to 'watched' list
+        user_data['watched'].append(movie_watched[0])
+
+        # Remove watched movie from 'watchlist' by filtering
+            # for titles that do not match 'movie_title'
+        # QUESTION: Is there a better way to do this?
+        movies_unwatched = list(filter(lambda movie: movie['title'] != movie_title, 
+            user_data['watchlist']))
+
+        # Refer 'watchlist' to updated list of 'movies_unwatched'
+        user_data['watchlist'] = movies_unwatched
+
+        return user_data
+    
+    else:
+        return user_data
