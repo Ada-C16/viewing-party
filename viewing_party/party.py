ae0041dddb17a05151d6ecdc1c23b6526170ca30
Rebecca Zuick <rebecca.zuick@gmail.com>
2021-09-11 09:33:44 -0500

    created add to watched function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -11,19 +11,12 @@ def create_movie(title, genre, rating):
 
 
 def add_to_watched(user_data, movie):
-    if user_data["watched"] == []:
-        user_data = None
+    
+    if movie:
+        user_data["watched"].append(movie)
     else:
-        user_data["watched"] = []
-        movie = {
-            "title": "Title A",
-            "genre": "Horror",
-            "rating": 3.5
-        }
-        (user_data["watched"]).append(movie)
+        user_data["watched"]    
     return user_data
-    # return user_data
-    # return user_data
 
 def add_to_watchlist():
     pass
