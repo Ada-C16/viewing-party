fc55a9c88e6d99c7b8dfeb3fb3b3f77f29cee0c3
Jesse <jesseannepope@gmail.com>
2021-09-11 16:25:02 -0700

    test changes for video

---
+++ viewing_party/party.py
@@ -0,0 +1,52 @@
+def create_movie(movie_title, genre, rating):
+    new_movie = {"title": movie_title, "genre": genre, "rating": rating}
+    return new_movie
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def create_movie(movie_title, genre, rating):
+#     if movie_title or 
+#     new_movie = {}
+#     new_movie["title"] = movie_title
+#     new_movie["genre"] = genre
+#     new_movie["rating"] = rating
+#     return new_movie
+
+# def add_to_watched(user_data, movie):
+#     pass
+
+# def add_to_watchllist(user_data, movie):
+#     pass
