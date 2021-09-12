d97c8b1e0c6df52475597d9301cac5f39439afa1
Shay <shay.phillips11@gmail.com>
2021-09-12 18:45:26 -0400

    Finished wave 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -26,3 +26,33 @@ def watch_movie(user_data, title):
             new_watchlist.append(movie)
     user_data["watchlist"] = new_watchlist
     return user_data
+
+def get_watched_avg_rating(user_data):
+    ratings = make_avg_rating_list(user_data)
+    num_ratings = len(ratings)
+    if num_ratings > 0:
+        avg_rating = sum(ratings)/num_ratings
+        return avg_rating
+    else:
+        return 0.0
+
+def make_avg_rating_list(user_data):
+    ratings = []
+    for movie in user_data["watched"]:
+        ratings.append(movie["rating"])
+    return ratings
+
+def get_most_watched_genre(user_data):
+    if not user_data["watched"]:
+        return None
+    else:
+        watched_genres = make_watched_genre_list(user_data)
+        most_watched_genre = max(watched_genres, key = watched_genres.count)
+        return most_watched_genre
+
+def make_watched_genre_list(user_data):
+    watched_genres = []
+    for movie in user_data["watched"]:
+        watched_genres.append(movie["genre"])
+    return watched_genres
+
