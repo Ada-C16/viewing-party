9ac792d3452e28b4b01783f59ef2bba6df9a0389
Shay <shay.phillips11@gmail.com>
2021-09-12 17:30:36 -0400

    Finished wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,9 +1,28 @@
 def create_movie(title, genre, rating):
     if title and genre and rating:
-        return {
+        new_movie = {
             "title": title,
             "genre": genre,
             "rating": rating
         }
+        return new_movie
     else:
         return None
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data    
+
+def watch_movie(user_data, title):
+    new_watchlist = []
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watched"].append(movie)
+        else:
+            new_watchlist.append(movie)
+    user_data["watchlist"] = new_watchlist
+    return user_data
