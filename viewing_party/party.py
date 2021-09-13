cafd32f570e10484dea40c87ddfb158895a6f16c
Gabe Kelemen <gabe@gabekelemen.com>
2021-09-13 13:49:07 -0400

    changes thru wave 3, no tests passed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -51,8 +51,73 @@ def watch_movie(user_data, title):
     user_data["watchlist"].pop(index_to_be_removed)
     
   return user_data
+
+
+
+    
+#### WAVE 2
+
+def get_watched_avg_rating(user_data):
+  user_rating_list = []
+  average = 0.0
+
+  for temp_movie_dict in user_data["watched"]:
+    user_rating_list.append(temp_movie_dict["rating"])
+  
+  if user_rating_list == []:
+    return average
+  else:
+    average = sum(user_rating_list) / len(user_rating_list)
+    return average
+  
+get_watched_avg_rating(user_data)
     
     
+def get_most_watched_genre:(user_data):
+  popular_genre_list = []
+  count = 0
+  
+  for temp_movie_dict in user_data["watched"]:
+    user_rating_list.append(temp_movie_dict["genre"])
+    
+    for i in popular_genre_list:
+        if popular_genre_list.count(i) > count:
+            count = popular_genre_list.count(i)
+    popular_genre = (popular_genre_list[count])
+
+
+get_most_watched_genre:(user_data)
+
+#### WAVE 3
+
+
+# movies friends have watched but user has not, return list of dictionaries
+
+def get_unique_watched(user_data):
+    user_movies_list = []
+    friends_movies_list = []
+    
+    for temp_movie_dict in user_data["watched"]:
+        user_movies_list.append(temp_movie_dict["title"])
+        
+    for tempt_movie_dict in user_data["friends"]:
+        for friend_dict in tempt_movie_dict["watched"]:
+            # for title in friend_dict["title"]:
+                friends_movies_list.append(friend_dict["title"])
+        
+        # for title in tempt_movie_dict:       
+        #     friends_movies_list.append(tempt_movie_dict[title])
+    # if not "friends" in user_data:
+    #     user_data["friends"] = []
+    print(user_movies_list)
+    print(friends_movies_list)
+
+
+get_unique_watched(user_data)
+  
+        
+  
+
   
 #watch_movie(user_data, title)
 #################################################
@@ -74,3 +139,34 @@ def watch_movie(user_data, title):
 # user_data = watch_movie(user_data, "Title Z")
 # print()
 # # movie = create_movie(movie_title, genre, rating)
+
+# user_data = {"watchlist": [{"title": "Babe",
+# "genre": "comedy", "rating": 1},{"title": "Startrek","genre":"scifi","rating": 1},{"title":"Cheers","genre": "drama","rating": 1} ], "watched": [{"title": "Starwars", "genre": "scifi","rating": 5}, {"title":"Matrix","genre": "scifi","rating": 3},{"title" : "Billboard","genre":"drama","rating": 1}]}
+
+# user_data3 = {"watchlist": [{"title": "Babe",
+# "genre": "comedy", "rating": 1},{"title": "Startrek","genre":"scifi","rating": 1},{"title":"Cheers","genre": "drama","rating": 1} ], "watched": [{"title": "Starwars", "genre": "scifi","rating": 5}, {"title":"Matrix","genre": "scifi","rating": 3},{"title" : "Billboard","genre":"drama","rating": 1}], "friends": [
+#             {
+#                 "watched": [
+#                     {
+#                         "title": "argosy"
+#                     },
+#                     {
+#                         "title": "startrek"
+#                     }
+#                 ]
+#             },
+#             {
+#                 "watched": [
+#                     {
+#                         "title": "Babe"
+#                     },
+#                     {
+#                         "title": "Cheers"
+#                     },
+#                     {
+#                         "title": "starwars"
+#                     }
+#                 ]
+#             }
+#         ]}
+
