05854c88fb1a9df83d4312c00553ca5ec4fcf919
Rhyannon <rhyannonjoy@gmail.com>
2021-09-13 10:05:10 -0600

    Added Wave 1-2 Pseudocode

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,2 +1,31 @@
-def create_movie():
+# Wave 1
+def create_movie(title, genre, rating):
+    # If title, genre, rating are truthy
+    # return a dictionary
+
+    # 3 k:v pairs
+    # 3 keys: "title", "genre", "rating"
+    # if title is falsy, genre falsy, or rating falsy
+    # return None
     pass
+    # movie = {}
+    # conditions to check if title, genre, rating are truthy
+    # maybe do this with bool()
+
+
+def add_to_watched(user_data, movie):
+    pass
+    # return user_data
+
+
+def add_to_watchlist(user_data, movie):
+    pass
+    # return user_data
+
+
+def watch_movie(user_data, title):
+    pass
+    # return user_data
+
+
+# Wave 3
