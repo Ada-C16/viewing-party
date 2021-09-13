7ce1259c45ce2a583e038eb9b8490e16d82db8d2
Rebecca Zuick <rebecca.zuick@gmail.com>
2021-09-13 17:24:08 -0500

    updated project with get unique watched function. passes first two tests in wave 3

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,15 +1,13 @@
 def create_movie(title, genre, rating):
-  if title and genre and rating:
-    new_movie = {}
-    new_movie["title"] = title
-    new_movie["genre"] = genre
-    new_movie["rating"] = rating
-  else: 
-    new_movie = None
-  return new_movie
+    if title and genre and rating:
+        new_movie = {}
+        new_movie["title"] = title
+        new_movie["genre"] = genre
+        new_movie["rating"] = rating
+    else: 
+        new_movie = None
+    return new_movie
     
-
-
 def add_to_watched(user_data, movie):
     if movie:
         user_data["watched"].append(movie)
@@ -23,7 +21,6 @@ def add_to_watchlist(user_data, movie):
     else:
         user_data["watchlist"]
     return user_data
-
 
 def watch_movie(user_data, title): 
     for item in user_data["watchlist"]:   
@@ -57,3 +54,21 @@ def get_most_watched_genre(user_data):
             genre = item["genre"]
     return genre
         
+def get_unique_watched(user_data):
+    movies_friends_watched = []
+    unique_movies = []
+    friend = []
+    watched = []
+    user = user_data["watched"]
+    for item in user_data["friends"]: # list of dictionaries
+        friend.append(item) 
+        for item in friend: 
+            watched.append(item["watched"])
+        for friend_list in watched:
+            for movie in friend_list:
+                movies_friends_watched.append(movie)
+    # print(f"{movies_friends_watched}=")
+    for item in user:
+        if item not in movies_friends_watched:
+            unique_movies.append(item)
+    return unique_movies
