ce533553c486ccf8aad2f2529e5fd5ad16a141f5
starseed2021 <tiff.harper18@gmail.com>
2021-09-14 12:04:23 -0400

    On Wave 4, Only Have 3 test case, Looking for #4

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -37,26 +37,26 @@ def watch_movie(user_data, title):
 
 #WAVE 2
 def get_watched_avg_rating(user_data):
-    list_of_watched_movies = user_data["watched"]
+    list_of_user_watched_movies = user_data["watched"]
     score = 0
     average_rating = 0.0
 
-    if list_of_watched_movies == []:
+    if list_of_user_watched_movies == []:
         return average_rating 
-    for movie in list_of_watched_movies:
+    for movie in list_of_user_watched_movies:
         score += movie["rating"]
-    average_rating = score/len(list_of_watched_movies)
+    average_rating = score/len(list_of_user_watched_movies)
     return average_rating
     
 
 def get_most_watched_genre(user_data):
-    list_of_watched_movies = user_data["watched"]
+    list_of_user_watched_movies = user_data["watched"]
     list_of_genres = []
 
-    if list_of_watched_movies == []:
+    if list_of_user_watched_movies == []:
             return None
     
-    for movie in list_of_watched_movies:
+    for movie in list_of_user_watched_movies:
         list_of_genres.append(movie["genre"])
 
     popular_genre = max(list_of_genres,key=list_of_genres.count)
@@ -69,7 +69,6 @@ def get_unique_watched(user_data):
     friend_movie_list = []
     unique_movie_list = []
 
-
     for friend in user_data["friends"]:
         for movie in friend["watched"]:
             friend_movie_list.append(movie) 
@@ -77,23 +76,20 @@ def get_unique_watched(user_data):
         if movie not in friend_movie_list:
             unique_movie_list.append(movie)
 
-    
     return unique_movie_list 
 
 
 def get_friends_unique_watched(user_data):
-    pass
     list_of_user_watched_movies = user_data["watched"]
-    friend_movie_list = []
+    friends_movie_list = []
     friends_unique_movie_list = []
     final_friends_unique_list = []
 
-
     for friend in user_data["friends"]:
         for movie in friend["watched"]:
-            friend_movie_list.append(movie) 
+            friends_movie_list.append(movie) 
 
-    for movie in friend_movie_list:
+    for movie in friends_movie_list:
         if not movie in list_of_user_watched_movies:
             friends_unique_movie_list.append(movie)
         
@@ -103,9 +99,74 @@ def get_friends_unique_watched(user_data):
 
     return final_friends_unique_list
 
+
 #WAVE 4
 def get_available_recs(user_data):
-    pass
+    # LIST OF MOVIES WATCHED BY THE USER
+    list_of_user_watched_movies = user_data["watched"]
+
+
+
+    #LIST OF SUBSCRIPTIONS OF THE USER
+    list_of_user_subscriptions = user_data["subscriptions"]
+
+    #LOOP TO GET SUBSCIPTONS OF THE FRIENDS
+    list_of_friends_subscriptions = []
+    friends_movie_list = []
+    friends_unique_movie_list = []
+    final_friends_unique_list = []
+
+
+    #LIST OF RECOMMENDED MOVIES
+    list_of_rec_movies = []
+
+    #CONDITIONAL FOR EMPTY LIST
+    if len(list_of_rec_movies) == 0:
+        return []
+
+    #LOOP GETS ALL THE MOVIES IN FRIENDS LIST
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            friends_movie_list.append(movie) 
+
+    #LOOP GETS THE MOVIES IN FRIENDS LIST THAT USER HAS NOT SEEN
+    for movie in friends_movie_list:
+        if not movie in list_of_user_watched_movies:
+            friends_unique_movie_list.append(movie)
+
+    #LOOP GETS ALL THE FRIENDS UNIQUE MOVIES WITHOUT DUPLICATES  
+    for movie in friends_unique_movie_list:
+        if not movie in final_friends_unique_list:
+            final_friends_unique_list.append(movie)
+
+    #LOOP GETS THE FRIENDS SUBSCRIPTIONS
+    for friend in user_data["friends"]:
+        for sub in friend["watched"]:
+          list_of_friends_subscriptions.append(sub)
+
+    #LOOP COMPARES THE FRIENDS SUBS WITH THE USERS
+    for movie in final_friends_unique_list:
+        if list_of_user_subscriptions == list_of_friends_subscriptions:
+            list_of_rec_movies.append(movie)
+
+    #RETURNS THE LIST OF RECOMMENEDED MOVIES
+    return list_of_rec_movies
+
+
+    
+    #FINAL FRIENDS UNIQUE LIST GIVES THE LIST OF MOVIES THAT USER HAS NOT SEEN
+    #LOOP THROUGH BOTH THE USER AND FRIENDS SUBSCRIPTIONS TO GET COMPARISON
+    #APPEND TO RECOMMENDED MOVIE LIST 
+    #RETURN THE LIST
+
+
+
+
+
+
+
+
+
 
 #WAVE 5
 def get_new_rec_by_genre(user_data):
