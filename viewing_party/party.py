f933a411e30bdbf5787f9509ea1a889e2489cac3
Rhyannon <rhyannonjoy@gmail.com>
2021-09-13 23:18:36 -0600

    test_wave_02 avg rating

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -38,4 +38,32 @@ def watch_movie(user_data, title):
             break
     return user_data
 
+
 # Wave 2
+def get_watched_avg_rating(user_data):
+    watched = user_data["watched"]
+
+    if len(watched) > 0:
+        rating_sum = 0.0
+        for movie in watched:
+            for key, value in movie.items():
+                if key == "rating":
+                    rating_sum += value
+                average_rating = rating_sum / len(movie)
+    if len(watched) == 0:
+        average_rating = 0.0
+    
+    return average_rating
+
+
+def get_most_watched_genre(user_data):
+    pass
+    # determined most_watched
+    # return most_watched
+
+
+# Wave 3
+# Wave 4
+# Wave 5
+
+
