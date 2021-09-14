5cfe4276c1a612110073165849d8f11d36bf644c
Rhyannon <rhyannonjoy@gmail.com>
2021-09-13 21:17:24 -0600

    Passed test_wave_01.py

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,31 +1,41 @@
 # Wave 1
 def create_movie(title, genre, rating):
-    # If title, genre, rating are truthy
-    # return a dictionary
-
-    # 3 k:v pairs
-    # 3 keys: "title", "genre", "rating"
-    # if title is falsy, genre falsy, or rating falsy
-    # return None
-    pass
-    # movie = {}
-    # conditions to check if title, genre, rating are truthy
-    # maybe do this with bool()
+    if title and genre and rating:
+        movie_dict = {
+            "title": title,
+            "genre": genre,
+            "rating": rating,
+             }
+        return movie_dict
+    else:
+        return None
 
 
 def add_to_watched(user_data, movie):
-    pass
-    # return user_data
+    if movie:
+        user_data["watched"].append(movie)
+    else:
+        user_data["watched"] = []
+    return user_data
 
 
 def add_to_watchlist(user_data, movie):
-    pass
-    # return user_data
+    if movie:
+        user_data["watchlist"].append(movie)
+    else:
+        user_data["watchlist"] = []
+    return user_data
 
 
 def watch_movie(user_data, title):
-    pass
-    # return user_data
+    watchlist = user_data["watchlist"]
+    watched = user_data["watched"]
+    
+    for movie in watchlist:
+        if title in movie["title"]:
+            watchlist.remove(movie)
+            watched.append(movie)
+            break
+    return user_data
 
-
-# Wave 3
+# Wave 2
