44683562d3e2dfc26b191f4b5dd5e4409331c022
Shay <shay.phillips11@gmail.com>
2021-09-14 01:24:39 -0400

    Updated Wave 01 watch_movie function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -20,13 +20,11 @@ def add_to_watchlist(user_data, movie):
     return user_data    
 
 def watch_movie(user_data, title):
-    new_watchlist = []
     for movie in user_data["watchlist"]:
         if movie["title"] == title:
-            user_data["watched"].append(movie)
-        else:
-            new_watchlist.append(movie)
-    user_data["watchlist"] = new_watchlist
+            add_to_watched(user_data, movie)
+            user_data["watchlist"].remove(movie)
+            break
     return user_data
 
 # Wave 02
@@ -121,4 +119,6 @@ def check_host_in_subs(user_data, host):
         return True
     else:
         return False
+
+# Wave 05
 
