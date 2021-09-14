2022ac9f36d1e94f7c92e8281a81a941e2eb29e5
kaitlyngore <kaitlyngore@gmail.com>
2021-09-14 19:05:08 -0400

    passes wave 1 and 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,3 +1,4 @@
+# wave 1
 def create_movie(title, genre, rating):
     if title == None or genre == None or rating == None:
         return None
@@ -19,3 +20,37 @@ def watch_movie(user_data, title):
             user_data["watched"].append(user_data["watchlist"][i])
             del user_data["watchlist"][i]
     return user_data
+
+# wave 2
+def get_watched_avg_rating(user_data):
+    average_rating = 0.0
+    movies_watched = len(user_data["watched"])
+    rating_numerator = 0
+    for i in range(len(user_data["watched"])):
+        rating_numerator += user_data["watched"][i]["rating"]
+    if len(user_data["watched"]) == 0:
+        return average_rating
+    else:
+        average_rating = (rating_numerator / movies_watched)
+        return average_rating
+
+def get_most_watched_genre(user_data):
+    genre_list = []
+    genre_dict = {}
+    most_frequent = ""
+    frequency = 0
+    if len(user_data["watched"]) == 0:
+        return None
+    for i in range(len(user_data["watched"])):
+        genre_list.append(user_data["watched"][i]["genre"])
+    for genre in genre_list:
+        if genre in genre_dict:
+            genre_dict[genre] += 1
+        else:
+            genre_dict[genre] = 1
+    else:
+        # reversed_genre_dict = {value: key for (key, value) in genre_dict.items()}
+        for key, value in genre_dict.items():
+            if value > frequency:
+                most_frequent = key
+        return most_frequent
