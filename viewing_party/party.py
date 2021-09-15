f72fcc6d8d1b3466085c9c98d4f096dee7f8317f
kristina <kkraft423@gmail.com>
2021-09-14 20:17:14 -0400

    Wave 2 first function completed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -13,33 +13,17 @@ def create_movie(title, genre, rating):
         return None
 
 
-create_movie("Lost Boys", "drama", 5)
-
 # ********************
     # PART TWO #
 # ********************
 
-user_info = { 
-    
-    "watched" : 
-    [{"title": "Title A",
-    "genre": "Horror",
-    "rating": 3.5}],
-    "watchlist":
-    [{"title": "Rocky Horror",
-    "genre": "Musical",
-    "rating": 5},
-    {"title": "Anchor Man",
-    "genre": "Comedy",
-    "rating": 3.8},
-    ]
-}
 
-movie_dict = {
-    "title": "The Shining",
-    "genre": "Horror",
-    "rating": 5
-}
+
+# movie_dict = {
+#     "title": "The Shining",
+#     "genre": "Horror",
+#     "rating": 5
+# }
 
 def add_to_watched(user_data, movie):
     if "watched" in user_data.keys():
@@ -47,9 +31,7 @@ def add_to_watched(user_data, movie):
     else:
         user_data["watched"] = [movie]
 
-    # print(user_data)
-
-add_to_watched(user_info, movie_dict)
+    return user_data
 
 # ********************
     # PART THREE #
@@ -57,13 +39,12 @@ add_to_watched(user_info, movie_dict)
 
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
-    print(user_data)
+    
+    return user_data
 
 # ********************
     # PART FOUR #
 # ********************
-
-
 
 def watch_movie(user_data, title):
 
@@ -76,7 +57,6 @@ def watch_movie(user_data, title):
             watched_movie = user_data["watchlist"][count-1]
             user_data["watchlist"].remove(watched_movie)
             user_data["watched"].append(watched_movie)
-            print("moved")
             movie_watched = True
             return user_data
         else: 
@@ -85,8 +65,46 @@ def watch_movie(user_data, title):
     if movie_watched == False:
         return user_data
 
+# WAVE 2 #
+
+user_info = { 
+    
+    "watched" : 
+    [{"title": "Title A",
+    "genre": "Horror",
+    "rating": 3.5},
+    {"title": "The Shining",
+    "genre": "Horror",
+    "rating": 5}],
+    "watchlist":
+    [{"title": "Rocky Horror",
+    "genre": "Musical",
+    "rating": 5},
+    {"title": "Anchor Man",
+    "genre": "Comedy",
+    "rating": 3.8},
+    ]
+}
+
+def get_watched_avg_rating(user_data):
+    # calculate the avg ratings of all movies in watchlist
+    # if watchlist is empty, the average rating is 0.0
+    # the data structure is a dictionary where the keys include "watched", etc, and the values are a list of dictionaries
+    sum = 0.0
+    num_of_ratings = 0
+    for watched in user_data["watched"]:
+        if user_data["watched"] == []:
+            return 0.0
+        for key, value in watched.items():
+            if key == "rating":
+                sum += value
+                num_of_ratings += 1
+
+    if num_of_ratings > 0:
+        average = sum / num_of_ratings
+        return average
+    else: 
+        return 0.0
+
             
    
-
-watch_movie(user_info, "Anchor Man")
-print(user_info)
