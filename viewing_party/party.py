101683b3cd073962c3c0851114d75e21e8533033
Rhyannon <rhyannonjoy@gmail.com>
2021-09-14 20:34:43 -0600

    Refactored Wave 1, passed Wave 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,3 +1,5 @@
+import statistics
+
 # Wave 1
 def create_movie(title, genre, rating):
     if title and genre and rating:
@@ -20,11 +22,17 @@ def add_to_watched(user_data, movie):
 
 
 def add_to_watchlist(user_data, movie):
-    if movie:
-        user_data["watchlist"].append(movie)
-    else:
-        user_data["watchlist"] = []
+    user_data["watchlist"].append(movie)
     return user_data
+    
+
+
+# def watch_movie(user_data,title): 
+#     for i,d in enumerate(user_data['watchlist']):
+#        if d['title'] == title:
+#           user_data['watched'].append(d)
+#           del user_data['watchlist'][i]      
+#     return user_data
 
 
 def watch_movie(user_data, title):
@@ -36,34 +44,51 @@ def watch_movie(user_data, title):
             watchlist.remove(movie)
             watched.append(movie)
             break
+    
     return user_data
 
 
 # Wave 2
 def get_watched_avg_rating(user_data):
     watched = user_data["watched"]
-
-    if len(watched) > 0:
-        rating_sum = 0.0
-        for movie in watched:
-            for key, value in movie.items():
-                if key == "rating":
-                    rating_sum += value
-                average_rating = rating_sum / len(movie)
-    if len(watched) == 0:
-        average_rating = 0.0
+    rating_sum = 0
     
+    if len(watched) == 0:
+        return 0
+    for movie in watched:
+        rating_sum += movie["rating"]  
+        average_rating = rating_sum / len(watched)
     return average_rating
 
 
 def get_most_watched_genre(user_data):
-    pass
-    # determined most_watched
-    # return most_watched
+    most_watched = user_data["watched"]
+    genres = []
+    if len(most_watched) == 0:
+        return None
+    for movie in most_watched:
+        genres.append(movie["genre"])
+    return statistics.mode(genres)
 
 
 # Wave 3
+def get_unique_watched(user_data):
+    pass
+
+
+
+
+
+def get_friends_unique_watched(user_data):
+    pass
+
+
+
 # Wave 4
+
 # Wave 5
+def get_new_rec_by_genre(user_data):
+    pass
+
 
 
