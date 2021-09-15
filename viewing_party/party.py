a6cd20e54d22fd968e646b90e1ace071f63fdde4
Monica Cruz <mcatcruz@gmail.com>
2021-09-15 17:20:36 -0400

    Finished Wave 3. Started Wave 4.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -66,18 +66,42 @@ def get_most_watched_genre(user_data):
     return most_watched_genre
 
 # Wave 3
+# This function consolidates friends' watched list values into one list. 
+def get_movie_list_from_watched_dict(list):
+    all_movies_watched_by_friends = []
+    for friend_movie_list in list:
+        for i in range(len(friend_movie_list["watched"])):
+            popped_movie = friend_movie_list["watched"].pop()
+            all_movies_watched_by_friends.append(popped_movie)
+    return all_movies_watched_by_friends
+
 def get_unique_watched(user_data):
     unique_movies = []
     watched = user_data["watched"]
-    friends = user_data["friends"]
-    for watched_by_user in watched:
-        # print(watched_by_user)
-        for watched_by_friends in friends:
-            for watched_by_friends_key, watched_by_friends_value in watched_by_friends.items():
-                # print(watched_by_friends_value, type(watched_by_friends_value), len(watched_by_friends_value))
-                for watched_by_friend_value in watched_by_friends_value:
-                    # print(watched_by_friend_value)
-                    if watched_by_user == watched_by_friend_value:
-                        print(watched_by_user, watched_by_friend_value)
-                    else:
-                        unique_movies.append(watched_by_user)
+    friends_list = user_data["friends"]
+    friends_movie_list = get_movie_list_from_watched_dict(friends_list)
+    for movie in watched:
+        if movie in friends_movie_list:
+            continue
+        else:
+            unique_movies.append(movie)
+    return unique_movies
+
+def get_friends_unique_watched(user_data):
+    friends_unique_movies = []
+    watched = user_data["watched"]
+    friends_list = user_data["friends"]
+    friends_movie_list = get_movie_list_from_watched_dict(friends_list)
+    for movie in friends_movie_list:
+        if movie in watched or movie in friends_unique_movies:
+            continue 
+        else:
+            friends_unique_movies.append(movie)
+    return friends_unique_movies
+
+# Wave 4
+
+
+
+def get_available_recs(user_data):
+    pass
