9611eca542cbce462ab28b20b5d1bd7ead94fffa
kaitlyngore <kaitlyngore@gmail.com>
2021-09-15 12:09:16 -0400

    passes test waves 1 2 3 4 and part of 5

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,4 +1,7 @@
 # wave 1
+from typing import ItemsView
+
+
 def create_movie(title, genre, rating):
     if title == None or genre == None or rating == None:
         return None
@@ -110,5 +113,47 @@ def get_available_recs(user_data):
         i = possible_recommendations.index(item)
         final_list.append({"title": possible_recommendations[i], "host": movie_host_list[i]})
     return final_list
-    # need to return it as a dictionary with title and host
-    # list.append({key: value}) ??
+
+# wave 5
+def get_new_rec_by_genre(user_data):
+    most_watched_genre = get_most_watched_genre(user_data)
+
+    possible_recommendations = []
+    user_movie_list = []
+    final_list = []
+    movie_genre_list = []
+    recommendation_dict = {}
+    for i in range(len(user_data["watched"])):
+        user_movie_list.append(user_data["watched"][i]["title"])
+    for friend in range(len(user_data["friends"])):
+        for movie in range(len(user_data["friends"][friend]["watched"])):
+            if user_data["friends"][friend]["watched"][movie]["genre"] == most_watched_genre:
+                possible_recommendations.append(user_data["friends"][friend]["watched"][movie]["title"])
+                movie_genre_list.append(user_data["friends"][friend]["watched"][movie]["genre"])
+        
+    recommendations = set(possible_recommendations) - set(user_movie_list)
+    if recommendations == False:
+        return None
+    for item in recommendations:
+        i = possible_recommendations.index(item)
+        final_list.append({"title": possible_recommendations[i], "genre": movie_genre_list[i]})
+    return final_list
+    
+# def get_rec_from_favorites(user_data):
+#     possible_recommendations = []
+#     user_movie_list = []
+#     final_list = []
+#     movie_genre_list = []
+#     recommendation_dict = {}
+#     for i in range(len(user_data["favorites"])):
+#         user_movie_list.append(user_data["favorites"][i]["title"])
+#     for friend in range(len(user_data["friends"])):
+#         for movie in range(len(user_data["friends"][friend]["watched"])):
+#             possible_recommendations.append(user_data["friends"][friend]["watched"][movie]["title"])
+                 
+#     recommendations = set(possible_recommendations) - set(user_movie_list)
+#     if recommendations == False:
+#         return None
+#     for item in recommendations:
+#         final_list.append({"title": item})
+#     return final_list
