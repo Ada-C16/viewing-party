336291451976cd2fc7bb456689509e790f8e2784
Rhyannon <rhyannonjoy@gmail.com>
2021-09-14 21:54:16 -0600

    Passed Wave 3-4, Will try to refactor

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -73,18 +73,96 @@ def get_most_watched_genre(user_data):
 
 # Wave 3
 def get_unique_watched(user_data):
-    pass
+    user_watched = user_data["watched"]
+    friends_watched = user_data["friends"]
+    
+    friends_titles = []
+    for friend in friends_watched:
+        for title in friend["watched"]:
+            friends_titles.append(title['title'])
 
+    unique_watched = []
 
+    for title in user_watched:
+        if title["title"] not in friends_titles:
+            unique_watched.append(title)
 
+    return unique_watched
+    
+    
 
+    
 
 def get_friends_unique_watched(user_data):
-    pass
+    user_watched = user_data["watched"]
+    friends_watched = user_data["friends"]
+    
+    user_titles = [title["title"] for title in user_watched]
+    friends_unique_watched = []
 
+    for friend in friends_watched:
+        for title in friend["watched"]:
+            if title["title"] not in user_titles:
+                if title not in friends_unique_watched:
+                    friends_unique_watched.append(title)
+
+    return friends_unique_watched
 
 
 # Wave 4
+# user has not watched
+def user_has_watched(movie, watched_movies):
+    if movie in watched_movies:
+        return True
+    else:
+        return False
+
+# at least 1 friend has watched it
+def in_friends_watchlist(movie, watched_by_friends):
+    if movie in watched_by_friends:
+        return True
+    else:
+        return False
+
+# host of the movie is a service that is a service subscription
+def in_users_subscriptions(host, subscriptions):
+    if host in subscriptions:
+        return True
+    else:
+        return False
+
+
+def get_friends_movies(user_data):
+    titles = set()
+    friends = user_data["friends"]
+    movies = []
+    for friend in friends:
+        for movie in friend["watched"]:
+            if movie["title"] not in titles:
+                movies.append(movie)
+                titles.add(movie["title"])
+    return movies
+
+
+def get_available_recs(user_data):
+    user_watched = user_data["watched"]
+    friends_movies = get_friends_movies(user_data)
+    available_recs = []
+
+    if len(friends_movies) == 0:
+        return []
+
+    user_watched_titles = set()
+    for movie in user_watched:
+        user_watched_titles.add(movie["title"])
+
+    for movie in friends_movies:
+        if movie["title"] not in user_watched_titles:
+            if in_users_subscriptions(movie["host"], user_data["subscriptions"]):
+                available_recs.append(movie)
+
+    return available_recs
+
 
 # Wave 5
 def get_new_rec_by_genre(user_data):
