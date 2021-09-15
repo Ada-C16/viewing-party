10ac2536eddb244683f82a2078369a683b73da8f
Reid Maute <reidhowdy@gmail.com>
2021-09-14 19:37:54 -0700

    wave 4

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -122,8 +122,6 @@ def get_unique_watched(user_data):
 
     only_user_watched_set = user_watched_set.difference(friends_watched_set)
 
-    print(only_user_watched_set)
-
     for item in only_user_watched_set:
         only_user_watched.append({"title" : item})
 
@@ -131,14 +129,131 @@ def get_unique_watched(user_data):
     return only_user_watched
                     
     
-#my logic doesn't make sense! I am comparing everything in one list to another
-#i can try to use sets instead
-#make two sets: one with everything from user,
-#one with everything from friends
-#then, compare sets
-#then, build a dict with only the values that are 
+
+#oart 2 of wave 3!
+
+def get_friends_unique_watched(user_data):
+    user_watched_set = set()
+    friends_watched_set = set()
+    only_friends_watched_set = set()
+    only_friends_watched = []
+
+
+    for movie_user_watched in user_data["watched"]:
+        user_watched_set.add(movie_user_watched["title"])
+        
+    for friends_watched_lists in user_data["friends"]:
+        for movies_friends_watched_list in friends_watched_lists.values():
+            for indiv_dict in movies_friends_watched_list:
+                friends_watched_set.add(indiv_dict["title"])
+
+    only_friends_watched_set = friends_watched_set.difference(user_watched_set)
 
     
+    for item in only_friends_watched_set:
+        only_friends_watched.append({"title" : item})
+
+    return only_friends_watched
+
+
+
+
+#wave 4!!!
+
+#i want to return a list of movies that:
+#the user hasn't watched - get_unique_watched()
+#rememeber that that^ get's me the ones they have watched
+
+#at least one of the friends has watched - get_friends_unique_watched()
+#user has not watched AND one of friends watched
+#AND the "host" is in "subscriptions"
+
+
+def get_available_recs(user_data):
+
+    #use my get_friends_unique_watched to get list of movies friends watched
+    #compare to hosts
+    #compare to subscription list
+    # return a list of dictionaries (with title and host as keys for each dict)
+
+    friends_watched_list = get_friends_unique_watched(user_data)
+    friends_watched_list_titles = []
+    
+    print(friends_watched_list)
+
+
+    for item in friends_watched_list:
+        friends_watched_list_titles.append(item["title"])
+
+
+    print(friends_watched_list_titles)
+
+
+    rec_list = []
+
+    for watched_lists in user_data["friends"]:
+        for watched_dict in watched_lists["watched"]:
+            if (watched_dict["host"] in user_data["subscriptions"]) \
+                and (watched_dict not in rec_list) \
+                and watched_dict["title"] in friends_watched_list_titles:
+                    rec_list.append(watched_dict)
+    print(rec_list)
+
+
+    return rec_list
+
+
+
+
+
+
+
+
+
+    """
+    for friends_watched_dicts in user_data["friends"]:
+        for one_list in friends_watched_dicts.values():
+            for one_dict in one_list:
+                complete_friends_watched_list.append(one_dict)
+    #print(complete_friends_watched_list)
+    #need list of movies NOT on user list, but on friends list
+    
+    
+    for indiv_friends_dict in complete_friends_watched_list:
+        #print(indiv_friends_dict["title"])
+        for indiv_user_dict in user_data["watched"]:
+            #print(indiv_user_dict["title"])
+            if indiv_friends_dict["title"] not in indiv_user_dict.values():
+                if indiv_friends_dict not in just_friends_watched:
+                    
+                    just_friends_watched.append(indiv_friends_dict)
+
+    print(just_friends_watched)
+    """
+
+
+    
+
+
+
+    #for user_movie_dict in user_movie_dict_list:
+        #i will want to use this to compare: user_movie_dict["title"]
+
+
+
+
+
+
+        
+
+
+    
+
+
+
+
+
+
 
 
 
