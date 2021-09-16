fb3e8e9746cd94cfcf56e1257c51b5a75f802471
Nicole Washington <nicolewashington.003@gmail.com>
2021-09-16 04:12:20 -0700

    added four functions and passed test for wave 01

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,31 @@
 def create_movie(title,genre,rating):
-    pass
+    new_movie = {}
+    empty = None
+    if title and genre and rating:
+        new_movie["title"] = title
+        new_movie["genre"] = genre
+        new_movie["rating"] = rating
+        return new_movie
+    else:
+        return empty
 
 
 
-def add_to_watch():
-    pass
+def add_to_watched(user_data,movie):
+    user_list = user_data["watched"]
+    user_list.append(movie)
+    return user_data
+
+
+def add_to_watchlist(user_data,movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data,title):
+    watch_list = user_data["watchlist"]
+
+    for movie in watch_list:
+        if title in movie.values():
+            watched_movie = watch_list.pop()
+            user_data["watched"].append(watched_movie)
+    return user_data
