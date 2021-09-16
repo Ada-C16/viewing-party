3c1c82507496743f25b3ea43d3782e54efba6c86
Jesse <jesseannepope@gmail.com>
2021-09-16 08:52:01 -0700

    added doc strings

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,4 +1,8 @@
 def create_movie(movie_title, genre, rating):
+    '''
+    functions builds a dictionary with three key value 
+    pairs where each value is the relevant parameter for each movie
+    '''
     if movie_title == None or genre == None or rating == None:
         return None
     new_movie = {
@@ -11,21 +15,34 @@ def create_movie(movie_title, genre, rating):
 
 
 def add_to_watched(user_data, movie):
+    '''
+    function adds a specific movie title to the users 
+    list of watched movies
+    '''
     user_data["watched"].append(movie)
     return user_data
 
 
 def add_to_watchlist(user_data, movie):
+    '''
+    function adds a specific movie title to the users 
+    list of movies to wacth
+    '''
     user_data["watchlist"].append(movie)
     return user_data
 
 
 def watch_movie(user_data, title):
+    '''
+    function checks if movie is in users watchlist. if so, adds 
+    movie to users watched list and removes it from users watchlist. 
+    returns updated user lists
+    '''
     movies_to_remove = []
     for movie in user_data["watchlist"]:
         if movie["title"] == title:
             movies_to_remove.append(movie)
-            
+
     if len(movies_to_remove) == 0:
         return user_data
     else:
@@ -37,6 +54,10 @@ def watch_movie(user_data, title):
 
 
 def get_watched_avg_rating(user_data):
+    '''
+    function gets average rating for the movies that are in 
+    users list fo watched movies
+    '''
     ratings = []
     if len(user_data["watched"]) == 0:
         return 0.0
@@ -67,6 +88,11 @@ def get_most_watched_genre(user_data):
 
 
 def get_unique_watched(user_data):
+    '''
+    function returns list of movies that are in users watched list
+    but not in any of the users friends watched list. the movies
+    in the returned list are unique to the user.
+    '''
     friends_watched = get_friends_watched_list(user_data)
     unique_movies = []
     for movie in user_data["watched"]:
@@ -76,6 +102,11 @@ def get_unique_watched(user_data):
 
 
 def get_friends_unique_watched(user_data):
+    '''
+    function returns list of movies that are in user's friends 
+    watched list but not in the users watched list. the movies 
+    in the returned list are unique to the user's friends.
+    '''
     friends_watched = get_friends_watched_list(user_data)
     friends_unique_movies = []
     for movie in friends_watched:
@@ -85,6 +116,12 @@ def get_friends_unique_watched(user_data):
 
 
 def get_available_recs(user_data):
+    '''
+    functions gets movie recommendations for user based on the
+    movies that the user's friends have watched. function checks if
+    the movie is in a subscription that the user has and the user 
+    has not watched the movie yet. returns list of recommendations.
+    '''
     recommendations = []
     user_subscriptions = user_data["subscriptions"]
     friends_watched = get_friends_watched_list(user_data)
@@ -101,6 +138,13 @@ def get_available_recs(user_data):
 
 
 def get_new_rec_by_genre(user_data):
+    '''
+    functions gets movie recommendations for user based on the
+    genre of movies that the user has watched and the movies of that 
+    specific genre that user's friends have watched. function checks if
+    the movie is not in the users watched list.
+    returns list of recommendations by genre.
+    '''
     user_genre_watched = []
     recommendations = []
 
@@ -121,6 +165,13 @@ def get_new_rec_by_genre(user_data):
 
 
 def get_rec_from_favorites(user_data):
+    '''
+    functions gets movie recommendations for user based on the
+    list of user favorites. function checks if the movie is not 
+    in the user's friends watched list. returns list of 
+    recommendations that are a user favorite and have not been 
+    watched by user's frineds.
+    '''
     user_favorites = []
     recommendations = []
     if len(user_data["favorites"]) > 0:
@@ -137,6 +188,11 @@ def get_rec_from_favorites(user_data):
     return recommendations
 
 def get_friends_watched_list(user_data):
+    '''
+    function creates list of movies watched by user's friends. 
+    returns list of dictionaries where key is title and value 
+    is movie title.
+    '''
     friends_watched = []
 
     for friend in user_data["friends"]:
