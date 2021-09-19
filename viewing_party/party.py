188b6706d84a9cfe8248ca9c53395aeabfe5627f
Lilly <waterlilly169@GMAIL.COM>
2021-09-19 00:13:19 -0700

    lilly pine edit1.1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -21,26 +21,21 @@ def add_to_watchlist(user_data, movie):
 def watch_movie(user_data, title):
   for movie_data in user_data["watchlist"]:
     if movie_data ["title"] == title:
-      user_data["watched"].append(movie_data)
+      add_to_watched(user_data, movie_data)
       user_data["watchlist"].remove(movie_data)
   return user_data 
 
 #Wave two
 def get_watched_avg_rating(user_data):
-    all_movie_ratings = []
-
-    for each_rating in user_data["watched"]:
-        all_movie_ratings.append(each_rating["rating"])
-
-    if len(all_movie_ratings) == 0:
-      return 0.0
-
-    average_rating_of_all = sum(all_movie_ratings)/len(all_movie_ratings)
-    return average_rating_of_all
+    all_movie_ratings = [each_rating["rating"] for each_rating in user_data["watched"]]
+    try:
+        average_rating_of_all = sum(all_movie_ratings)/len(all_movie_ratings)
+        return average_rating_of_all
+    except ZeroDivisionError:
+        return 0 
 
 def get_most_watched_genre(user_data):
   genre_map = {}
-
   for genre in user_data["watched"]:
     if genre["genre"] not in genre_map:
         genre_map[genre["genre"]] = 1 
@@ -55,106 +50,42 @@ def get_most_watched_genre(user_data):
 
 #Wave Three
 def get_unique_watched(user_data):
-    #tyring out list/dictionary comprehension structure
-    user_watched_list = {y['title']: y['title'] for friends_watched in user_data['friends'] for y in friends_watched['watched']}
-    unique_data = [r for r in user_data['watched'] for k, v in r.items() if v not in user_watched_list.keys()]
-
-    return unique_data  
+    user_watched_list = {y['title'] for friends_watched in user_data['friends'] for y in friends_watched['watched']}    
+    unique_data = [r for r in user_data['watched'] if r['title'] not in user_watched_list]
+    return unique_data
 
 def get_friends_unique_watched(user_data):
-    x = 1
-    user_watched_list = {watched['title']: watched['title'] for watched in user_data['watched']}
-    unique_data = [y for x in user_data['friends'] for y in x['watched'] if y.values() in user_watched_list.keys()]
-    return unique_data 
+    user_watched_list = []
+    for friends_watched in user_data['friends']: 
+        for y in friends_watched['watched']: 
+            if y not in user_data['watched'] and y not in user_watched_list and y['title'] not in user_watched_list:
+                user_watched_list.append(y)
+    return user_watched_list 
 
-def get_friends_unique_watched(user_data):
-    user_watched_list = {}
-    unique_data = []
-  
-    x = 1
-    for watched in user_data['watched']:
-        user_watched_list[watched['title']] = watched['title']
-
-    for each_friend in user_data['friends']:
-        if len(each_friend['watched']) > 0:
-            for x in each_friend['watched']:
-                for k, v in x.items():
-                    if v not in user_watched_list.keys():
-                        unique_data.append(v)
-           
-    set1 = set(unique_data)
-    list1 = list(set1)
-    final = []
-    for x in list1:
-        dict1 = {'title': x}
-        final.append(dict1)
-
-    return final  
-
-#wave 4 
-                      
+#Wave 4                
 def get_available_recs(user_data):
-    user_dict_sub = {}
-    user_dict_watched = {}
     recomendations = []
-
-    #creating dictionaires for user data
-    counter = 0
-    for subscription in user_data['subscriptions']:
-        user_dict_sub[counter] = subscription 
-        counter += 1
-    x = 0
-    for watched in user_data['watched']:
-        for k, v in watched.items():
-            user_dict_watched[x] = v
-            x += 1
-
-    #getting recomendations
+    watches = [x['title'] for x in user_data['watched']]
     for friend in user_data['friends']:
         for watch in friend['watched']:
-            if watch['host'] in user_dict_sub.values() and watch['title'] not in user_dict_watched.values() and watch not in recomendations:
+            if watch['host'] in user_data['subscriptions'] and watch['title'] not in watches and watch not in recomendations:
                 recomendations.append(watch)
+
     return recomendations
 
 #Wave five
 def get_new_rec_by_genre(user_data):
-    genre_map = {}
-    if len(user_data['watched']) == 0:
-        return []
-    else:
+    max_key = get_most_watched_genre(user_data)
+    users_watched_set = {title['title'] for title in user_data['watched']}
+    recomendations = []
 
-        for genre in user_data["watched"]:
-            if genre["genre"] not in genre_map:
-                genre_map[genre["genre"]] = 1 
-            else:
-                genre_map[genre["genre"]] += 1
-
-        max_key = max(genre_map, key=genre_map.get)
-        
-        users_watched_dict = {}
-        for title in user_data['watched']:
-            users_watched_dict[title['title']] = title['title']
-        
-        recomendations = []
-        for friends_watched in user_data['friends']:
-            for watch in friends_watched['watched']:
-                if watch['title'] not in users_watched_dict and watch['genre'] == max_key and watch not in recomendations:
-                    recomendations.append(watch)
-        return recomendations
+    for friends_watched in user_data['friends']:
+        for watch in friends_watched['watched']:
+            if watch['title'] not in users_watched_set and watch['genre'] == max_key and watch not in recomendations:
+                recomendations.append(watch)
+    return recomendations
 
 def get_rec_from_favorites(user_data):
-    if len(user_data['favorites']) == 0:
-        return []
-    else:
-        friend_watch_dict = {}
-        
-        for friend in user_data['friends']:
-            for friend_watch in friend['watched']:
-                friend_watch_dict[friend_watch['title']] = friend_watch['title']
-
-        recomendations = []
-        for fav in user_data['favorites']:
-            if fav['title'] not in friend_watch_dict:
-                recomendations.append(fav)
-
-        return recomendations
+    friend_watch_set = {friend_watch['title'] for friend in user_data['friends'] for friend_watch in friend['watched']}
+    recomendations = [fav for fav in user_data['favorites'] if fav['title'] not in friend_watch_set]
+    return recomendations
--- viewing_party/wildflower.py
+++ viewing_party/wildflower.py
@@ -1,1 +1,46 @@
+amandas_data = {
+    "subscriptions": ["Service A", "Service B"],
+    "watched": [{ "title": "Title A" }],
+    "friends": [
+        {
+            "watched": [
+                {
+                    "title": "Title A",
+                    "host": "Service A"
+                },
+                {
+                    "title": "Title C",
+                    "host": "Service C"
+                }
+            ]
+        },
+        {
+            "watched": [
+                {
+                    "title": "Title A",
+                    "host": "Service A"
+                },
+                {
+                    "title": "Title B",
+                    "host": "Service B"
+                },
+                {
+                    "title": "Title D",
+                    "host": "Service D"
+                }
+            ]
+        }
+    ]
+}
 
+def get_available_recs(user_data):
+    recomendations = []
+    watches = [x['title'] for x in user_data['watched']]
+    for friend in user_data['friends']:
+        for watch in friend['watched']:
+            if watch['host'] in user_data['subscriptions'] and watch['title'] not in watches and watch not in recomendations:
+                recomendations.append(watch)
+
+    return recomendations
+
+print(get_available_recs(amandas_data))
