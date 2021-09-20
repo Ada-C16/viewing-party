b41632f5a83664eecfd7d0b3b82293ac078c8919
Lizet Tovar <lizett503@gmail.com>
2021-09-19 17:54:18 -0700

    Added functions for Wave 5.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -2,7 +2,8 @@ from statistics import mode
 # The first four tests are going to test against this function:
 def create_movie(title, genre, rating):
     """
-    This function takes title, genre and ratings as keys and the input as their values. 
+    This function takes title, genre and ratings as parameters and returns 
+    the input arguments as values in a dictionary. 
     """
     new_movie = {}
     if title and genre and rating:
@@ -16,8 +17,8 @@ def create_movie(title, genre, rating):
 # The next two tests are about this function:
 def add_to_watched(user_data, movie):
     """
-    Input: user data where usand movie 
-    Output: 
+    This function takes movie info  and adds them to an already watched
+    list if the user has watched it. 
     """
     already_watched_list = user_data["watched"]
     already_watched_list.append(movie)
@@ -143,3 +144,56 @@ def get_available_recs(user_data):
     
     return recommended_movies
 
+# WAVE 5
+# Four tests for this function:
+def get_new_rec_by_genre(user_data):
+    """
+    This function will return a list  of recommendations based off 
+    the users most watched genres and if they have not seen the movie yet. 
+    """
+    recommended_movies = []
+
+    user_watched = user_data["watched"]
+    friends_watched = user_data["friends"]
+    
+    # list of dictionary title  & genre user has watched
+    movies_watched_by_user = []
+    for title in user_watched:
+        movies_watched_by_user.append(title)
+    
+    # list of movies friends have watched
+    friends_watched_movies = []
+    for friend in friends_watched:
+        for movie in friend["watched"]:
+            if movie not in movies_watched_by_user:
+                if movie not in friends_watched_movies:
+                    if movie["genre"] in get_most_watched_genre(user_data):
+                        recommended_movies.append(movie)
+    return recommended_movies
+
+
+# Last two tests are for this function:
+def get_rec_from_favorites(user_data):
+    """
+    This function will return a list of recommended movies based on the users 
+    favorite movies and if none of the users friends have watched it
+    """
+    favorite_recs = []
+
+    # this will hold list of movie dict that are users favorite 
+    users_favorites = user_data["favorites"]
+    friends_watched = user_data["friends"]
+    
+    # list of movies user has watched and friends have not
+    users_unique_movies = get_unique_watched(user_data)
+
+    # compare users favorite title to friends watched title 
+    for movie in friends_watched:
+        if movie["watched"] in users_favorites:
+            if users_unique_movies not in friends_watched:
+                favorite_recs.append(movie)
+    return favorite_recs
+
+
+
+   
